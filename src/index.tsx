b2bd2921555381ca8728c875ea017a11cb7f830f
Sang-Sang33 <2363481389@qq.com>
2023-03-07 18:24:21 +0800

    chore: add gif

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ export { default as useJsonStates } from './JsonForm/common/hooks/useJsonStates'
 export { default as useFormActions } from './JsonForm/common/hooks/useFormActions';
 export { default as JsonForm } from './JsonForm';
 export type { IFormItem } from './JsonForm/common/constants/type';
-export type { ETypes } from './JsonForm/common/constants/index';
+export { ETypes } from './JsonForm/common/constants/index';
